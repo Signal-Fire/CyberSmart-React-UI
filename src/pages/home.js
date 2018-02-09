51e7c7c167d04a16627c5878e27c0919ac54dfbc
Henry <henrypye@outlook.com>
2018-02-09 21:29:51 +0000

    Implemented router and split out pages into seperate views

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+/*jshint esversion:6*/
+import React, { Component } from 'react';
+import '../App.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="App">        
+                <p className="App-intro">
+                    Welcome to this app
+                </p>
+            </div>
+        )
+    }
+}
