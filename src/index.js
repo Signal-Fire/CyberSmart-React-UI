17f9aa672f95169dc4eadef4a78e70230a5b3585
Henry <henrypye@outlook.com>
2018-02-09 19:16:19 +0000

    Added Bootstrap into project and added imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 /*jshint esversion: 6*/
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
