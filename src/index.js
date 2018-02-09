3c160d52b385d744b009d42f0b31d813c3f7bc87
Henry <henrypye@outlook.com>
2018-02-09 13:01:11 +0000

    Tidied up templates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,4 +6,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
