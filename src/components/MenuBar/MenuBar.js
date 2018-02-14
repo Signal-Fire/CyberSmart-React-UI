d1c989c2cbaca25148aa2d237b761dfcb7f8f889
Henry <henrypye@outlook.com>
2018-02-14 14:06:42 +0000

    Created Exports File and folder to export components

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -4,8 +4,27 @@ import React, { Component } from 'react';
 export default class componentName extends Component {
   render() {
     return (
-      <div>
-        
+      <div class="ui secondary  menu">
+        <a class="active item">
+          Home
+        </a>
+        <a class="item">
+          Messages
+        </a>
+        <a class="item">
+          Friends
+        </a>
+        <div class="right menu">
+          <div class="item">
+            <div class="ui icon input">
+              <input type="text" placeholder="Search..." />
+              <i class="search link icon"></i>
+            </div>
+          </div>
+          <a class="ui item">
+            Logout
+          </a>
+        </div>
       </div>
     )
   }
