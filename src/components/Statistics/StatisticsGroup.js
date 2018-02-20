5a22bba087f40503dbaa27f2430c09f85235aa74
Henry Pye <henrypye@Hibflib.local>
2018-02-20 00:32:24 +0000

    Removed unused imports

--- src/components/Statistics/StatisticsGroup.js
+++ src/components/Statistics/StatisticsGroup.js
@@ -1,13 +1,12 @@
 /* jshint esversion: 6 */
 import React, {  Component  } from 'react';
-import {  Icon,  Statistic, Grid } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 import { StatisticsWidget } from '../Exports/Widgets/Exports';
 
 export default class StatisticsGroup extends Component {
   render() {
     return (
-      //<Statistic.Group widths = 'four' >
       <Grid relaxed columns = {4}>
        <Grid.Column>
           <StatisticsWidget icon = 'power' title = 'Devices ON' number = '1' />
@@ -22,7 +21,6 @@ export default class StatisticsGroup extends Component {
           <StatisticsWidget icon = 'warning' title = 'Errors' number = '0' />
         </Grid.Column>
       </Grid>
-      //</Statistic.Group>
     );
   }
 }
