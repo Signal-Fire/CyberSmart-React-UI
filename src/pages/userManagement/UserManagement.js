64a7e943ef8a12029eaf0a273a4444530f31cfff
deanolingardo <deanlingard@yahoo.co.uk>
2018-02-28 12:47:55 +0000

    Corrected User Management page error

--- src/pages/userManagement/UserManagement.js
+++ src/pages/userManagement/UserManagement.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 import '../Master.css';
 import './UserManagement.css';
 
-export default UserManagementPage extends Component {
+export default class UserManagementPage extends Component {
       render() {
           return (
               <header className="page-body">
