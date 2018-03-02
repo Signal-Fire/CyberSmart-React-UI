0166625ec3889ae8bcd99d5e243db4383e5aae1c
deanolingardo <deanlingard@yahoo.co.uk>
2018-03-02 14:25:55 +0000

    Changed tab style in the help page

--- src/components/Help/index.js
+++ src/components/Help/index.js
@@ -2,14 +2,17 @@
 import React from 'react';
 import { Tab } from 'semantic-ui-react';
 
+import { InfoAccordian } from '../../components/Exports';
+
+
 const panes = [
-  { menuItem: 'Setup', render: () => <Tab.Pane attached={true}>Put content here</Tab.Pane> },
-  { menuItem: 'Usage', render: () => <Tab.Pane attached={true}>Put content here</Tab.Pane> },
-  { menuItem: 'Navigating', render: () => <Tab.Pane attached={true}>Put Content Here</Tab.Pane> },
+  { menuItem: 'Guide', render: () => <Tab.Pane attached={true}>Coming soon!</Tab.Pane> },
+  { menuItem: 'FAQ', render: () => <Tab.Pane attached={true}>Coming soon!</Tab.Pane> },
+  { menuItem: 'Contact us', render: () => <Tab.Pane attached={true}>CyberSmart Team</Tab.Pane> },
 ]
 
 const HelpTabs = () => (
-  <Tab menu={{ secondary: true }} panes={panes} />
+  <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
 )
 
 export default HelpTabs
