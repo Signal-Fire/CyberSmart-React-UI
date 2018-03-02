b322fe83815aed8ec222e66cc95b6c9856da00d6
Henry <henrypye@outlook.com>
2018-03-02 10:58:09 +0000

    Sidebar is now fixed

--- src/pages/Exports/Exports.js
+++ src/pages/Exports/Exports.js
@@ -3,7 +3,7 @@ import DevicePage from '../Devices/Devices';
 import RoomPage from '../Rooms/Rooms';
 import HomePage from '../Home/Home';
 import HelpPage from '../Help/Help';
-import UserManagementPage from '../userManagement/UserManagement'
+import UserManagementPage from '../UserManagement';
 
 export {
     DevicePage,
