3620b2fddeef8c7ca41b16ce60c77036f017a12d
Henry <henrypye@outlook.com>
2018-03-05 22:06:55 +0000

    gitignore and menubar changes (small)

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -22,27 +22,34 @@ export default class MenuBar extends Component {
 
   render() {
     return (
-      <div className="ui inverted primary attached pointing menu">
-        <Menu.Item icon = 'sidebar' onClick = { this.toggleVisibility } />
-        <div className="left logo">
-          <div className= "item">
-            <p>CyberSmart</p>
+      <div className="ui inverted primary attached pointing menu borderless">
+      <Menu 
+        inverted
+        primary
+        attached
+        pointing
+        borderless>
+          <Menu.Item icon = 'sidebar' onClick = { this.toggleVisibility } />
+          <div className="left logo">
+            <div className= "item">
+              <p>CyberSmart</p>
+            </div>
           </div>
-        </div>
-        <div className="right menu">
-          <Dropdown item icon='announcement' pointing = 'top right'>
-            <Dropdown.Menu>
-              <MenuMessage message="You left x lights on this week!"/>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown item icon='settings' pointing = 'top right'>
-            <Dropdown.Menu>
-              <UserModal />
-              <HubModal />
-              <MenuIcon title = "Logout" icon = "sign out" onClick = {this.logout}/>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
+          <div className="right menu">
+            <Dropdown item icon='announcement' pointing = 'top right'>
+              <Dropdown.Menu>
+                <MenuMessage message="You left x lights on this week!"/>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown item icon='settings' pointing = 'top right'>
+              <Dropdown.Menu>
+                <UserModal />
+                <HubModal />
+                <MenuIcon title = "Logout" icon = "sign out" onClick = {this.logout}/>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </Menu>
       </div>
     )
   }
