bf0763aee936ef7acfce6e64cea1b6de0c2961c8
Henry <henrypye@outlook.com>
2018-03-06 02:04:51 +0000

    Finished user modal, now updates, very big file. not happy. clean? who knows? may need to revisit in the future but functions soooo yay

--- src/components/UserModal/index.js
+++ src/components/UserModal/index.js
@@ -4,22 +4,28 @@ import React, { Component } from 'react';
 import { USER_COOKIE_IDENTIFIER, API_USERS_URL } from '../../config';
 
 import { MenuIcon } from '../Exports';
-import { Modal, Button, Header, Form } from 'semantic-ui-react';
+import { Modal, Button, Header, Form, Message } from 'semantic-ui-react';
 
 import axios from 'axios';
 
 export default class UserModal extends Component {
     constructor() {
         super();
+
         this.state = {
             firstName : "",
             lastName : "",
-            hubUser : "",
-            hubPass : "",
+            username : "",
+            password : "",
+            loading: false,
+            changed: false,
             isOpen: false
         };
 
+        this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.getUserInformation = this.getUserInformation.bind(this);
+        this.updateUserInformation = this.updateUserInformation.bind(this);
     }
 
     getUserInformation() {
@@ -31,27 +37,64 @@ export default class UserModal extends Component {
         this.setState({
           firstName : this.upperCase(res.data.first_name),
           lastName : this.upperCase(res.data.last_name),
-          hubUser : res.data.username,
-          hubPass : "*******"
+          username : res.data.username,
+          password : "*******"
         });
       }).catch(err => {
+        console.error(err);
+      });
+    }
+
+    updateUserInformation() {
+      this.setState({ loading: true });
+      axios({ method: 'POST',
+          url: API_USERS_URL + '/update', 
+          headers: {
+            'Authorization' : localStorage.getItem(USER_COOKIE_IDENTIFIER)
+          }, 
+          data : {
+            username : this.state.username,
+            password : this.state.password,
+            first_name : this.state.firstName,
+            last_name : this.state.lastName
+          }                   
+      }).then(res => {
+        this.setState({ loading: false, changed: true });     
+      }).catch(err => {
+        this.setState({ loading: false });
         console.error(err);
       });
     }
    
     handleClick() {
         this.setState({
-            isOpen: !this.state.isOpen
+            isOpen: !this.state.isOpen,
+            changed: false
         });
+    }
+
+    handleChange(e) {
+      switch(e.target.name) {
+        case 'username':
+          this.setState({ username : e.target.value });
+          break;
+        case 'firstname':
+          this.setState({ firstName : e.target.value });
+          break;
+        case 'lastname':
+          this.setState({ lastName : e.target.value });
+          break;
+        case 'password':
+          this.setState({ password : e.target.value });
+        break;       
+        default:
+          break; 
+      }
     }
 
     upperCase(value) {
       return value.charAt(0).toUpperCase() + value.slice(1);
-    }
-
-    componentDidMount() {
-      this.getUserInformation();
-    }
+    } 
 
     render() {
       return (
@@ -59,6 +102,7 @@ export default class UserModal extends Component {
           dimmer={true}
           open={this.state.isOpen}
           onClose={this.handleClick}
+          onOpen={this.getUserInformation}
           size='small'
           closeOnEscape={true}
           closeOnRootNodeClick={false}
@@ -68,23 +112,50 @@ export default class UserModal extends Component {
           <Modal.Content>
             <Modal.Description>
               <Header>Edit your User Settings from here</Header>
-              <Form>
+              <Form loading = {this.state.loading}>
                     <Form.Group widths={2}>
-                        <Form.Input label='First Name' placeholder='First Name' value = {this.state.firstName}/>
-                        <Form.Input label='Last Name' placeholder='Last Name' value = {this.state.lastName} />
+                        <Form.Input 
+                          label = 'First Name'
+                          placeholder = 'First Name'
+                          name = 'firstname' 
+                          value = {this.state.firstName} 
+                          onChange = {this.handleChange}/>
+                        <Form.Input 
+                          label = 'Last Name' 
+                          placeholder = 'Last Name' 
+                          name = 'lastname'
+                          value = {this.state.lastName} 
+                          onChange = {this.handleChange}/>
                     </Form.Group>
                     <Form.Group widths={2}>
-                        <Form.Input label='Hub Username' placeholder='Hub Username' value = {this.state.hubUser}/>             
-                        <Form.Input label='Hub Password' placeholder='Hub Password' type = 'password' />
-                    </Form.Group>                    
+                        <Form.Input 
+                          label = 'Hub Username' 
+                          placeholder = 'Hub Username'
+                          name = 'username' 
+                          value = {this.state.username} 
+                          onChange = {this.handleChange}/>             
+                        <Form.Input 
+                          label='Hub Password' 
+                          placeholder='Hub Password' 
+                          name = 'password'
+                          type = 'password' 
+                          value = {this.state.password}
+                          onChange = {this.handleChange} />
+                    </Form.Group>              
                 </Form>
+                <Message
+                    success
+                    header='Success!'
+                    content="User information changed!"
+                    hidden={!this.state.changed}
+                  />  
             </Modal.Description>
           </Modal.Content>          
           <Modal.Actions>
             <Button color='black' onClick={this.handleClick}>
               Cancel
             </Button>
-            <Button positive icon='checkmark' labelPosition='right' content="Save" onClick={this.handleClick} />
+            <Button positive icon='checkmark' labelPosition='right' content="Save" onClick={this.updateUserInformation} />
           </Modal.Actions>
         </Modal>
       )
