9a5fcf42b207b372c4c3c652baa8dcf52e885d22
Dean Lingard <deanlingard@yahoo.co.uk>
2018-03-22 19:18:53 +0000

    removed sidebar and added links to menubar

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -2,13 +2,13 @@
 import React, { Component } from 'react';
 
 import { APP_NAME, USER_COOKIE_IDENTIFIER } from '../../config';
-import { Dropdown, Menu } from 'semantic-ui-react';
+import { Dropdown, Menu, Image, Container, Segment, Button, Icon } from 'semantic-ui-react';
 import { MenuIcon, MenuMessage, UserModal, HubModal } from '../Exports';
 
 export default class MenuBar extends Component {
   constructor(props) {
     super(props);
-    
+
     this.toggleVisibility = this.toggleVisibility.bind(this);
   }
 
@@ -21,34 +21,76 @@ export default class MenuBar extends Component {
     this.props.toggleVisibility();
   }
 
+
   render() {
+
     return (
-      <Menu 
-        inverted
-        attached
-        pointing
-        borderless>
-          <Menu.Item icon = 'sidebar' onClick = { this.toggleVisibility } />         
-          <div className="left logo">
-            <div className= "item">
-              <p>{APP_NAME}</p>
-            </div>
-          </div>
-          <div className="right menu">
-            <Dropdown item icon='announcement' pointing = 'top right'>                        
-              <Dropdown.Menu>                
-                <MenuMessage message="You left x lights on this week!"/>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown item icon='settings' pointing = 'top right'>
-              <Dropdown.Menu>
-                <UserModal />
-                <HubModal />
-                <MenuIcon title = "Logout" icon = "sign out" onClick = {this.logout}/>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </Menu>
+      <Menu size='small'
+            fixed='top'
+            borderless
+            pointing
+            >
+
+       <Menu.Item>
+       <Button.Group>
+            <Button animated='fade' basic>
+              <Button.Content visible>Home</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='home' />
+                </Button.Content>
+            </Button>
+
+            <Button animated='fade' basic>
+              <Button.Content visible>Devices</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='chart' />
+                </Button.Content>
+            </Button>
+
+            <Button animated='fade' basic>
+              <Button.Content visible>Rooms</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='block device' />
+                </Button.Content>
+            </Button>
+
+            <Button animated='fade' basic>
+              <Button.Content visible>Users</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='user' />
+                </Button.Content>
+            </Button>
+
+            <Button animated='fade' basic>
+              <Button.Content visible>Help</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='info' />
+                </Button.Content>
+            </Button>
+            </Button.Group>
+            </Menu.Item>
+
+
+         <Menu.Menu position='right'>
+           <Dropdown item text='Settings' Icon name='gamepad'>
+             <Dropdown.Menu>
+               <Dropdown.Item><HubModal /></Dropdown.Item>
+               <Dropdown.Item><UserModal /></Dropdown.Item>
+             </Dropdown.Menu>
+           </Dropdown>
+
+           <Menu.Item>
+           <Menu.Item>
+           <Button animated='fade' basic>
+             <Button.Content visible>Log out</Button.Content>
+               <Button.Content hidden>
+                 <Icon name='lock' icon='settings' />
+               </Button.Content>
+           </Button>
+           </Menu.Item>
+           </Menu.Item>
+         </Menu.Menu>
+       </Menu>
     )
   }
 };
