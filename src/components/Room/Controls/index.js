230be42eab9893ea52e06ab23949b9d598e765df
Henry <henrypye@outlook.com>
2018-03-28 23:13:21 +0100

    Added MenuBar to Location Control Panel

--- src/components/Room/Controls/index.js
+++ src/components/Room/Controls/index.js
@@ -1,17 +1,20 @@
 /* jshint esversion: 6 */
 import React from 'react';
+import { Menu, Input } from 'semantic-ui-react';
+import { API_LOCATIONS_URL } from '../../../config';
 
 import { AddLocationModal } from '../../Modals/Add';
-import { Card } from 'semantic-ui-react';
 
 const LocationControlPanel = () => (
-    <Card fluid>
-      <Card.Content>
-        <Card.Header>Control Panel</Card.Header>
-        <Card.Meta></Card.Meta>
-        <Card.Description><AddLocationModal /></Card.Description>
-      </Card.Content>
-    </Card>
+    <Menu inverted>
+          <Menu.Item name='home' />
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              
+            </Menu.Item>
+          </Menu.Menu>
+    </Menu>
+    
 );
 
 export default LocationControlPanel;
