04e128b164d2e1ffa52f83dda811cc10a0b1a979
Dean Lingard <deanlingard@yahoo.co.uk>
2018-04-01 09:33:13 +0100

    Inverted menubar, settings dropdown logout needs fixing

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import { MenuIcon, UserModal, HubModal } from '../Exports';
-
+import { APP_NAME, USER_COOKIE_IDENTIFIER } from '../../config';
 import { HashRouter, Link } from 'react-router-dom';
 import {  Container, Icon, Image, Menu, Sidebar, Responsive, Button, Dropdown } from "semantic-ui-react";
 
@@ -59,9 +59,10 @@ const NavBarMobile = ({
               borderless
               pointing
               attached
+              inverted
               >
         <Menu.Item>
-          <Image size="mini" src="assets/images/logo.png" />
+          <Image size="mini" src="assets/images/Menubarlogo.jpg" />
         </Menu.Item>
         <Menu.Item onClick={onToggle}>
           <Icon name="sidebar" />
@@ -88,38 +89,39 @@ const NavBarDesktop = ({ leftItems, rightItems }) => (
             fixed='top'
             borderless
             pointing
+            inverted
             >
 
     <Menu.Item>
-      <Image size="mini" src="assets/images/logo.png" />
-          CyberSmart
+      <Image size="mini" src="assets/images/Menubarlogo.jpg" />
+          {APP_NAME}
     </Menu.Item>
 
 <Menu.Item>
  <Button.Group pointing='top left'>
         <Button animated='fade' basic to="/" as={ Link }>
-          <Button.Content visible><Icon name='dashboard' /></Button.Content>
+          <Button.Content visible><Icon name='dashboard' inverted/></Button.Content>
             <Button.Content hidden>
               Home
             </Button.Content>
         </Button>
 
         <Button animated='fade' basic to="/devices" as={ Link }>
-          <Button.Content visible><Icon name='block layout' /></Button.Content>
+          <Button.Content visible><Icon name='block layout' inverted/></Button.Content>
             <Button.Content hidden>
               Devices
             </Button.Content>
         </Button>
 
         <Button animated='fade' basic to="/rooms" as={ Link }>
-          <Button.Content visible><Icon name='home' /></Button.Content>
+          <Button.Content visible><Icon name='home' inverted/></Button.Content>
             <Button.Content hidden>
               Rooms
             </Button.Content>
         </Button>
 
         <Button animated='fade' basic to="/help" as={ Link }>
-          <Button.Content visible><Icon name='info' /></Button.Content>
+          <Button.Content visible><Icon name='info' inverted/></Button.Content>
             <Button.Content hidden>
               Help
             </Button.Content>
@@ -142,16 +144,25 @@ const NavBarChildren = ({ children }) => (
   <Container style={{ marginTop: "4.5em" }}>{children}</Container>
 );
 
+
 export default class MenuBar extends Component {
   state = {
     visible: false
   };
+
 
   handlePusher = () => {
     const { visible } = this.state;
 
     if (visible) this.setState({ visible: false });
   };
+
+  logout() {
+     if (window.localStorage)
+       localStorage.removeItem(USER_COOKIE_IDENTIFIER);
+
+     window.location.reload();
+   }
 
   handleToggle = () => this.setState({ visible: !this.state.visible });
 
