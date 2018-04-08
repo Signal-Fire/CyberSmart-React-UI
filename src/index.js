f79f6dacf64326feb847d0039ebb410c668c96c9
Henry <henrypye@outlook.com>
2018-04-08 14:56:08 +0100

    Started adding redux

--- src/index.js
+++ src/index.js
@@ -7,5 +7,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+document.getElementById('root'));
 registerServiceWorker();
