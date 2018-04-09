02dc70ed8f0e0a9ffe3284bd549b2d51f1ac25f3
Henry <henrypye@outlook.com>
2018-04-09 14:10:20 +0100

    Working tests for Device Widget grid

---
+++ src/components/Device/Grid/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import Widget from '../Widget';
+import { connect } from 'react-redux';
+import { getDevices } from '../../../actions/Devices';
+
+class WidgetGrid extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            devices : []
+        }
+    }
+
+    componentDidMount = () => {
+        this.props.getDevices();
+    }   
+      
+    componentWillReceiveProps = (nextProps) => {
+        if (nextProps.newDevice) {
+            this.props.getDevices();
+        }
+    }
+
+    render() {
+        return(
+            <Grid stackable columns = {6}>                              
+                {this.props.devices.map((d) =>
+                    <Grid.Column key = {d._id}>  
+                        <Widget name = {d.name} state = {d.state} address = {d.address}/>
+                    </Grid.Column>
+                )}                        
+            </Grid>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        devices : state.deviceReducer.devices
+    }
+}
+
+export default connect(mapStateToProps, { getDevices })(WidgetGrid);
--- src/pages/Devices/index.js
+++ src/pages/Devices/index.js
@@ -1,11 +1,6 @@
 /*jshint esversion: 6*/
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { getDevices } from '../../actions/Devices';
-
-//Custom components import
-import { DeviceWidget } from '../../components/Exports/Widgets';
+import { DeviceGrid } from '../../components/Device';
 import { Panel } from '../../components/ControlPanel';
 
 //CSS import
@@ -20,38 +15,16 @@ class DevicePage extends Component {
             isLoading: true,
             devices: []
         };
-    }
-    
-    componentDidMount = () => {
-        this.props.getDevices();
-    }   
-      
-    componentWillReceiveProps = (nextProps) => {
-        if (nextProps.newDevice) {
-            this.props.getDevices();
-        }
-    }
+    }    
     
     render() {
         return (
             <header className="page-body">
-                    <Panel name = 'Devices' />                             
-                    <Grid stackable columns = {6}>                              
-                        {this.props.devices.map((d) =>
-                            <Grid.Column key = {d._id}>  
-                                <DeviceWidget name = {d.name} state = {d.state} address = {d.address}/>
-                            </Grid.Column>
-                        )}                        
-                    </Grid>
+                <Panel name = 'Devices' />                             
+                <DeviceGrid />
             </header>
         )
     }    
 }
 
-const mapStateToProps = state => {
-    return {
-        devices : state.deviceReducer.devices
-    }
-}
-
-export default connect(mapStateToProps, { getDevices })(DevicePage);
+export default DevicePage;
