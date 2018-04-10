f536fd7ac123db763afd0bae2dd0ceb1aa643eb2
Henry Pye <henrypye@outlook.com>
2018-04-10 09:34:54 +0100

    Added methods to create locations dropdown to Locations reducer

--- src/containers/Location/reducer.js
+++ src/containers/Location/reducer.js
@@ -17,3 +17,20 @@ export default function(state = initialState, action) {
             return state;
     }
 }
+
+export const getLocations = () => {
+    return this.state.locations
+}
+
+export const createLocationsDropdown = () => {
+    if (this.state.locations === null ||
+        this.state.locations.length < 1)
+        return null;
+
+    var data = this.state.locations.forEach(item => {           
+        item.key = item._id;  
+        item.text = item.name;
+        item.value = item._id;               
+    });
+    return data;
+}
