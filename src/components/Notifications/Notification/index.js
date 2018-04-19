30ed418b188cfc1b154e982adaa6e502ff547790
Henry <henrypye@outlook.com>
2018-04-19 11:10:14 +0100

    Reduxed cognitive complexity of Notification

--- src/components/Notifications/Notification/index.js
+++ src/components/Notifications/Notification/index.js
@@ -2,36 +2,36 @@ import React from 'react';
 import { Feed } from 'semantic-ui-react';
 import moment from 'moment';
 
+const capz = (word) => {
+    try {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    } catch (ex) {
+        return word;
+    }
+}
+
+const lower = (word) => {
+    try {
+        return word.charAt(0).toLowerCase() + word.slice(1);
+    } catch (ex) {
+        return word;
+    }
+}
+
+const iconSelector = (type) => {
+    switch(type) {
+        case 'Device':
+            return 'mobile';
+        case 'Location':
+            return 'home';
+        default:
+            return 'alarm';
+    }
+}
+
 const Notification = ({
     notification
-}) => {
-    const capz = (word) => {
-        try {
-            return word.charAt(0).toUpperCase() + word.slice(1);
-        } catch (ex) {
-            return word;
-        }
-    }
-
-    const lower = (word) => {
-        try {
-            return word.charAt(0).toLowerCase() + word.slice(1);
-        } catch (ex) {
-            return word;
-        }
-    }
-
-    const iconSelector = (type) => {
-        switch(type) {
-            case 'Device':
-                return 'mobile';
-            case 'Location':
-                return 'home';
-            default:
-                return 'alarm';
-        }
-    }
-
+}) => {  
     return(
         <Feed.Event
             icon ={iconSelector(notification.type)}
