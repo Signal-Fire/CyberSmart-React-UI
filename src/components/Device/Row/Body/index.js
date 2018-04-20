11b753bec150eb6f97b8f38542485d2e42b374cc
Henry <henrypye@outlook.com>
2018-04-20 23:12:49 +0100

    Remove button prints ID on click

--- src/components/Device/Row/Body/index.js
+++ src/components/Device/Row/Body/index.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
-import { CreatedTag, StateTag, StateButtons } from '../';
+import { CreatedTag, StateTag, StateButtons, DeleteButton } from '../';
 import { connect } from 'react-redux';
 
 const Row = ({
     device
 }) => {
     return (
-        <Table.Row 
-            
+        <Table.Row             
             positive = {device.state === 1}
-            negative = {device.state !== 1}>
-            <Table.Cell width = '3'>{device.name}</Table.Cell>            
-            <Table.Cell width = '3'>{device.location}</Table.Cell>            
-            <Table.Cell width = '3'><StateTag 
+            negative = {device.state !== 1}
+            >
+            <Table.Cell>{device.name}</Table.Cell>     
+            <Table.Cell>{device.address}</Table.Cell>          
+            <Table.Cell>{device.location}</Table.Cell>                  
+            <Table.Cell><StateTag 
                             state = {device.state} /></Table.Cell>
                             <Table.Cell><StateButtons 
                     state = {device.state}
                     id = {device._id}
                     address = {device.address}
                 /></Table.Cell>
-            <Table.Cell width = '3'><CreatedTag 
+            <Table.Cell width ='3'><CreatedTag 
                             name = {device.created_by_user} 
                             date = {device.date_added}/></Table.Cell>
+            <Table.Cell><DeleteButton id = {device._id} /></Table.Cell>
         </Table.Row>
     );
 }
--- src/components/Device/Row/index.js
+++ src/components/Device/Row/index.js
@@ -2,10 +2,12 @@ import DeviceRow from './Body';
 import CreatedTag from './CreatedTag';
 import StateTag from './StateTag';
 import StateButtons from './OperationButtons';
+import DeleteButton from './Delete';
 
 export {
     DeviceRow,
     CreatedTag,
     StateTag,
-    StateButtons
+    StateButtons,
+    DeleteButton
 }
