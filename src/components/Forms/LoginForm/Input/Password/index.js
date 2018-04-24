b9c34890312c346085997d0e6063a699487f61bd
Henry <henrypye@outlook.com>
2018-04-24 09:31:07 +0100

    Hotfix - iconPosition for password now on left hand side of password field

--- src/components/Forms/LoginForm/Input/Password/index.js
+++ src/components/Forms/LoginForm/Input/Password/index.js
@@ -7,6 +7,7 @@ const PasswordInput = ({
   return (
     <Form.Input
         icon='lock'
+        iconPosition = 'left'
         placeholder='Password'
         name='password'
         type='password'
