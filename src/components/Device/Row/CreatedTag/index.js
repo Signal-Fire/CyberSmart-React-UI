9feb2fb6a3036d91916b29f5e964b638d2493064
Henry Pye <henrypye@outlook.com>
2018-04-25 08:53:14 +0100

    Helpers file now has capz in it

--- src/components/Device/Row/CreatedTag/index.js
+++ src/components/Device/Row/CreatedTag/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Label } from 'semantic-ui-react';
 import moment from 'moment';
-
-const capz = (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1, word.length);
-}
+import * as helpers from '../../../../helpers';
 
 const CreatedByLabel = ({
     name,
@@ -12,7 +9,7 @@ const CreatedByLabel = ({
 }) => {
     return (
         <Label as = 'a' color = 'teal' image>
-            {capz(name)}   
+            {helpers.capz(name)}   
             <Label.Detail>{moment(date).format('MMMM Do, HH:mm')}</Label.Detail>     
         </Label>
     );
---
+++ src/helpers/index.js
@@ -0,0 +1,3 @@
+export const capz = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1, word.length);
+}
