55c7ec2fc34ef0ae24b213f4ad0cb1be3ee852f1
Henry <henrypye@outlook.com>
2018-05-01 21:01:31 +0100

    Final touches!

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,9 +1,7 @@
 import PowerChart from './PowerChart';
 import DeviceChart from './DeviceChart';
-import ConsumptionChart from './ConsumptionChart';
 
 export { 
   PowerChart,
-  DeviceChart,
-  ConsumptionChart
+  DeviceChart
 }
