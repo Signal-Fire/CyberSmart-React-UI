0287980782deff0b8cdb9acdb144b4189d5b10af
Henry <henrypye@outlook.com>
2018-05-28 11:12:27 +0100

    Created test suite for CreatedTag / Locations Body

--- src/components/Locations/Row/Body/BodyTest.test.js
+++ src/components/Locations/Row/Body/BodyTest.test.js
@@ -2,8 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Body from './';
 
-test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Body />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
+//Redux
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Devices Table Body Test Suite', () => {
+    test('renders without crashing with no devices and no locations', () => {
+        const initialState = {
+            locations : [],
+            devices : []
+        }
+        const store = mockStore( initialState );
+
+        const div = document.createElement('div');
+
+        ReactDOM.render(
+        <Provider store = { store }>
+            <Body />
+        </Provider>, div);
+
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    test('renders without crashing with devices and locations', () => {
+        const initialState = {
+            locations : [{
+                _id : '1',
+                active : true,
+                name : 'Living Room',
+                created_by_user : 'henry'
+            }],
+            devices : [{
+                active : true,
+                location : 'Living Room'
+            }]
+        }
+
+        const store = mockStore( initialState );
+
+        const div = document.createElement('div');
+
+        ReactDOM.render(
+        <Provider store = { store }>
+            <Body />
+        </Provider>, div);
+
+        ReactDOM.unmountComponentAtNode(div);
+    });
+})
--- src/components/Locations/Row/CreatedTag/CreatedTagTest.test.js
+++ src/components/Locations/Row/CreatedTag/CreatedTagTest.test.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Tag from './';
 
-test('renders without crashing', () => {
-    const user = {
-        signup_timestamp : '2018-05-05',
-        created_by_user : 'test suite'
-    }
+describe('Locations Created Tag Test Suite', () => {
+    test('renders without crashing', () => {
+        const location = {
+            signup_timestamp : '2018-05-05',
+            created_by_user : 'test suite'
+        }
 
-    const div = document.createElement('div');
-    ReactDOM.render(<Tag user = { user }/>, div);
-    ReactDOM.unmountComponentAtNode(div);
+        const div = document.createElement('div');
+        ReactDOM.render(<Tag location = { location }/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
