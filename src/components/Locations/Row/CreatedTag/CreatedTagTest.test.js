2321eacff73ee501d52c3dc993cb68d35eacb95a
Henry <henrypye@outlook.com>
2018-05-28 10:53:46 +0100

    Fixed CreatedTagTest for Locations

--- src/components/Locations/Row/CreatedTag/CreatedTagTest.test.js
+++ src/components/Locations/Row/CreatedTag/CreatedTagTest.test.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import Tag from './';
 
 test('renders without crashing', () => {
+    const user = {
+        signup_timestamp : '2018-05-05',
+        created_by_user : 'test suite'
+    }
+
     const div = document.createElement('div');
-    ReactDOM.render(<Tag />, div);
+    ReactDOM.render(<Tag user = { user }/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
