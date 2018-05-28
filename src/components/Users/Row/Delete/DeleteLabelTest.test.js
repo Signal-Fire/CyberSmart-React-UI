9d34825e2ca6d44664317f93118020e4c4075f48
Henry <henrypye@outlook.com>
2018-05-28 11:52:43 +0100

    Added Users Delete Label Test Suite, fixed tests

--- src/components/Users/Row/Delete/DeleteLabelTest.test.js
+++ src/components/Users/Row/Delete/DeleteLabelTest.test.js
@@ -2,8 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Delete from './';
 
-test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Delete />, div);
-    ReactDOM.unmountComponentAtNode(div);
+//Redux
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Users Delete Label Test Suite', () => {
+    test('renders without crashing', () => {
+        const initialState = {
+            login : {
+                token : null
+            },
+            user : {
+                first_name : ''
+            }
+        }
+
+        const store = mockStore(initialState);
+
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <Provider store = { store }>
+            <Delete id = '1' />
+        </Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
