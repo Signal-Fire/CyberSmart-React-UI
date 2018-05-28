bc3b4eefe25f55ea0c7560775604ca2872e76b6c
Henry <henrypye@outlook.com>
2018-05-28 12:00:49 +0100

    Fixed Device Stats and created test suite, lunchtime!

--- src/components/Dashlets/DeviceStats/DeviceStatsTest.test.js
+++ src/components/Dashlets/DeviceStats/DeviceStatsTest.test.js
@@ -7,16 +7,43 @@ import { API_DEVICES_URL } from '../../../config';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-test('renders without crashing', () => {
-    var Mock = new MockAdapter(axios);
+//Redux
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-    var data = {
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
-    };
+describe('Device Stats test', () => {
+    test('renders without crashing', () => {
+        const initialState = {
+            locations : {
+                locations : []
+            },
+            devices : {
+                devices : []
+            },
+            user : {
+                all_users : []
+            }
+        }
 
-    Mock.onGet(API_DEVICES_URL + '/find/all').reply(200, data);
-    
-    const div = document.createElement('div');
-    ReactDOM.render(<AddLocationModal />, div);
-    ReactDOM.unmountComponentAtNode(div);
+        const store = mockStore(initialState);
+        
+        var Mock = new MockAdapter(axios);
+
+        var data = {
+
+        };
+
+        Mock.onGet(API_DEVICES_URL + '/find/all').reply(200, data);
+        
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <Provider store = { store }>
+            <AddLocationModal />
+        </Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
