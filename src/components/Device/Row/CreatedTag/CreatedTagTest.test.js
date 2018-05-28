ed888b8f1e0c3d6c0b26d039ba96f7de4e459505
Henry <henrypye@outlook.com>
2018-05-28 11:05:08 +0100

    Created Devices Created Tag test suite, fixed tests

--- src/components/Device/Row/CreatedTag/CreatedTagTest.test.js
+++ src/components/Device/Row/CreatedTag/CreatedTagTest.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Tag from './';
 
-test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Tag />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Devices Created Tag Test Suite', () => {
+    test('renders without crashing', () => {
+        const device = {
+            created_by_user : 'henry',
+            date_added : 'now'
+        }
+
+        const div = document.createElement('div');
+
+        ReactDOM.render(<Tag device = { device } />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
