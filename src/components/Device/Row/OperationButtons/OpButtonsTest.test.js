ae2ab646d19a8a5162e9d006ea5682a5e67db3c4
Henry <henrypye@outlook.com>
2018-05-28 11:40:50 +0100

    Fixed Operation Buttons Tests and created a test suite

--- src/components/Device/Row/OperationButtons/OpButtonsTest.test.js
+++ src/components/Device/Row/OperationButtons/OpButtonsTest.test.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import OpBtns from './';
 
-test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<OpBtns />, div);
-    ReactDOM.unmountComponentAtNode(div);
+//Redux
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Operations Buttons Test Suite', () => { 
+    test('renders without crashing', () => {
+        const initialState = {
+            devices : {
+                devices : [],
+                isLoading : false
+            }
+        }
+        const store = mockStore(initialState);
+
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <Provider store = { store }>
+            <OpBtns />
+        </Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
