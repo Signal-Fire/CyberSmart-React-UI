a2253788a86115f22cdb0ac08c4607cc9fbec2ca
Henry <henrypye@outlook.com>
2018-05-28 22:15:09 +0100

    Replaced front page charts to use custom charts container

--- src/components/Charts/PowerChart/index.js
+++ src/components/Charts/PowerChart/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from '../Container';
 import { Header } from 'semantic-ui-react';
-import { LineChart, Line, Legend, Tooltip, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { CustomChart } from '../CustomChart';
 
 const data = [
     {kwh: 0.89, time: '21:30'},
@@ -18,18 +18,12 @@ const BasicChart = ( { header }) => {
     return (
         <div>
             <Header as ='h3' textAlign='center'>{ header }</Header>
-            <Container>
-                <LineChart data={data}
-                        margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                        <XAxis dataKey= 'time'/>
-                        <YAxis dataKey = 'kwh'/>
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <Tooltip/>
-                        <Legend />
-                        <Line type="monotone" dataKey='kwh' stroke="#8884d8" activeDot={{r: 8}}/>
-                        <Line type="monotone" dataKey='time' stroke="#82ca9d" />
-                </LineChart>
-            </Container>
+            <CustomChart 
+                data = {data}
+                dataKeys = {{
+                        X : 'time',
+                        Y : 'kwh'
+                }} />
         </div>
     );
 }
