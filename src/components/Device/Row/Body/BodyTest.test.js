568b43f85e425ec0f4cbea4bdaa3f4ecfe7a8972
Henry <henrypye@outlook.com>
2018-05-28 11:42:13 +0100

    Created Device Table Body Test Suite, fixed tests

--- src/components/Device/Row/Body/BodyTest.test.js
+++ src/components/Device/Row/Body/BodyTest.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Body from './';
 
-test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Body />, div);
-    ReactDOM.unmountComponentAtNode(div);
+//Redux
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Device Table Body Test Suite', () => {
+    test('renders without crashing', () => {
+        const initialState = {
+            devices : {
+                devices : []
+            }
+        }
+        const store = mockStore(initialState);
+
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <Provider store = {store}>
+            <Body />
+        </Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
