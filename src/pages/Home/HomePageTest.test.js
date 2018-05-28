9a28abf3d7ec37d95b59b568b14e0d482a179004
Henry <henrypye@outlook.com>
2018-05-28 13:09:05 +0100

    Created Home Page Test Suite and fixed tests

--- src/pages/Home/HomePageTest.test.js
+++ src/pages/Home/HomePageTest.test.js
@@ -7,11 +7,38 @@ import { API_DEVICES_URL } from '../../config';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-test('renders without crashing', () => {
-    var Mock = new MockAdapter(axios);
-    Mock.onGet(API_DEVICES_URL + '/find/all').reply(200);
+//Redux
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-    const div = document.createElement('div');
-    ReactDOM.render(<HomePage />, div);
-    ReactDOM.unmountComponentAtNode(div);
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Home Page Test Suite', () => {
+    test('renders without crashing', () => {
+        const initialState = {
+            locations : {
+                locations : []
+            },
+            devices : {
+                devices : []
+            },
+            user : {
+                all_users : []
+            }
+        }
+
+        const store = mockStore(initialState);
+
+        var Mock = new MockAdapter(axios);
+        Mock.onGet(API_DEVICES_URL + '/find/all').reply(200);
+
+        const div = document.createElement('div');
+        ReactDOM.render(
+        <Provider store = {store}>
+            <HomePage />
+        </Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
