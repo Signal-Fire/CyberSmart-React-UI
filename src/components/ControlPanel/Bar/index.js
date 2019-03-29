ab19007c7c208daf57b1815c14501fe2e5257529
Henry Pye <henrypye@outlook.com>
2019-03-29 13:47:23 +0000

    Added redundancy for if no users added

--- src/components/ControlPanel/Bar/index.js
+++ src/components/ControlPanel/Bar/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
+import './controlPanelBar.css';
+
 const Bar = ( { children }) => {
     return(
         <Menu 
+        className = 'control-panel-bar'
         inverted
         size = 'small'>
             {children}
